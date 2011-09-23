3d85e41b45cca6e4aa3d65256d15ea2f2d9dcd75
Ben Truyman <bentruyman@gmail.com>
2011-09-22 21:57:32 -0500

    Frontend router now renders pages properly

--- public/javascripts/admin.js
+++ public/javascripts/admin.js
@@ -1,16 +1,43 @@
-Sammy(function () {
-  // get channels
-  this.get('/admin/channels/', function () {
-    API.channels.read().then(function (data) {
-      // inject channels data into channels template
-      $('#channels').append(
-        $('#template-channels').tmpl(data)
-      );
-    });
-  });
+(function () {
+  var containerSelector = '#main';
   
-  // get channels
-  this.get('/admin/channels/update/:id', function () {
-    console.log('Hello World');
-  });
-}).run();
+  Sammy(containerSelector, function (app) {
+    var $main = $(containerSelector);
+    
+    // page renderer helper
+    app.helper('renderPage', function () {
+      var deferred = $.Deferred(),
+          self = this;
+      
+      // load this current page, and inject the content of its main container
+      // into the current page's main container
+      $.ajax(this.path).then(function (html) {
+        var mainContainer = $(html).filter(containerSelector).get(0);
+        self.$element().html(mainContainer.innerHTML);
+        deferred.resolve();
+      });
+      
+      return deferred.promise();
+    });
+    
+    // get channels
+    this.get('/admin/channels/', function () {
+      this.renderPage().then(function () {
+        // load all channels
+        API.channels.read().then(function (data) {
+          // inject channels data into channels template
+          $('#channels').append(
+            $('#template-channels').tmpl(data)
+          );
+        });
+      });
+    });
+    
+    // get channels
+    this.get('/admin/channels/update/:id', function () {
+      this.renderPage().then(function () {
+        console.log('Hello World');
+      });
+    });
+  }).run();
+}());
